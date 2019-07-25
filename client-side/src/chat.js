c30863aee2cd068e6ea18421be9e7dbf4e3447e1
Roysan Easo <easo.roysan@gmail.com>
2019-07-25 14:46:03 -0500

    moved necessary documents to client-side

---
+++ client-side/src/chat.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import socketIO from 'socket.io-client'
+
+const io = socketIO('http://localhost:8080')
+
+export class Chat extends React.Component{
+
+    state={
+        messages: [],
+        newMessage: ""
+    }
+      
+    render(){
+        return (
+            <div>
+            {this.state.messages.map( (eachMessage) => <p key={eachMessage.id} >{eachMessage.message}</p> )}
+            <form onSubmit={(e)=> this.handleSubmit(e)}>
+                <input name="newMessage" type="text" value={this.state.newMessage} onChange={(e)=>this.setState({ newMessage: e.target.value })} />
+                <input type="Submit" />
+            </form>
+            </div>
+        )
+    }
+
+    componentDidMount(){
+
+        io.emit('messages/index',{}, messages=>{
+            console.log(messages)
+            this.setState({ messages: messages })
+        })
+
+        io.on('newMessage', newMessage =>{
+            this.setState({ messages: [...this.state.messages, newMessage]})
+        })
+
+        io.on('startingHand', startingHand =>{
+            console.log(startingHand)
+        })
+    }
+    
+    handleSubmit(e){
+        e.preventDefault()
+
+        if(this.state.newMessage.split(" ").join("").length > 0){
+            io.emit('sentMessage',{message: this.state.newMessage}, newMessage =>{
+                this.setState({
+                    messages: [...this.state.messages, newMessage],
+                    newMessage: ""
+                })
+            })
+        }
+    }
+
+}
---
+++ client-side/src/login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import socketIO from 'socket.io-client'
+
+const io = socketIO('http://localhost:8080')
+
+export class Login extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <h1>Log In First BIIIITCH</h1>
+            </div>
+        )
+    }
+}
