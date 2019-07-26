f2e774ec5e07c29fe0ec0152931287a9eea43c73
Jason Fournier <jmf4nier@hotmail.com>
2019-07-26 09:16:14 -0500

    nothing new

--- server/models/message.js
+++ server/models/message.js
@@ -4,6 +4,9 @@ const sqlDatabase = require('../database')
 const Message = sqlDatabase.define( 'message', {
     message: {
         type: Sequelize.STRING
+    },
+    userId: {
+        type: Sequelize.INTEGER
     }
 })
 
