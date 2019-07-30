324f7449cf98ee30f4d40bceee52c925a88fbc6e
Roysan Easo <easo.roysan@gmail.com>
2019-07-30 09:47:34 -0500

    fixed chat problems after merging with roysan

--- client/src/chat.js
+++ client/src/chat.js
@@ -12,7 +12,7 @@ export class Chat extends React.Component{
                 <div className="comment">
                     <div className="content">
                         <div className="text">
-                             {this.state.messages.map( (eachMessage) =>
+                             {this.props.chatMessages.map( (eachMessage) =>
                                 <p key={eachMessage.id} >
                                 <strong>{eachMessage.username}</strong>: 
                                 {" " + eachMessage.message}</p> 
@@ -25,7 +25,7 @@ export class Chat extends React.Component{
                 </div>
                 <form className="ui reply form" onSubmit={(e)=> this.props.handleSubmit(e)}>
                     <div className="field" >
-                        <input name="newMessage"  type='text' value={this.state.newMessage} onChange={(e)=>this.props.handleChange(e)}></input>
+                        <input name="newMessage"  type='text' value={this.props.newMessage} onChange={(e)=>this.props.handleChange(e)}></input>
                     </div>
                     <input type="Submit"  className="ui blue button"/>
                 </form>
